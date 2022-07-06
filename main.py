476f297e9868275613b9a01265ff93b8f5f12580
Nikolaka17 <nikolakakealoha@gmail.com>
2022-07-06 17:53:39 -0500

    Add grid to background

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from sys import exit
 class Window:
     def __init__(self):
         self.width = 800
-        self.height = 600
+        self.height = 630
 
         pygame.init()
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.screen.fill((0, 0, 0))
         self.clock = pygame.time.Clock()
         self.fps = 60
+    
+    def draw_grid(self):
+        for x in range(250, self.width - 250, 30):
+            for y in range(15, self.height - 15, 30):
+                pygame.draw.rect(self.screen, (255, 255, 255), (x, y, 30, 30), 1)
 
     def run(self):
         while True:
@@ -19,6 +24,7 @@ class Window:
                     pygame.quit()
                     exit()
             
+            self.draw_grid()
             pygame.display.update()
             self.clock.tick(self.fps)
 
